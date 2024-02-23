2e50a4e17ba2260b844de3855afa4a8e2de1fca3
NSIX06 <felipebugalho2016@gmail.com>
2024-02-23 11:53:12 -0300

    menu em desenvolvimento

---
+++ assets/js/menu.js
@@ -0,0 +1,19 @@
+function handleMenu() {
+    const menu = document.querySelector(".menu");
+    const opacity = document.querySelector(".menu__opacity");
+    
+
+    if (menu.style.display == "none") {
+        menu.style.display = "block";
+        opacity.style.backgroundColor = "rgba(0, 0, 0, 0.60)";
+        menu.style.zIndex = "10";
+    
+    } else {
+        menu.style.display = "none";
+        opacity.style.backgroundColor = "#0000";
+        menu.style.zIndex = "0";
+
+
+
+    }
+}
