f7cccb94ca2dd8727e92e5cb004183013c12fbee
NSIX06 <felipebugalho2016@gmail.com>
2024-03-01 11:51:59 -0300

    mobileCursos 100%

--- login.php
+++ login.php
@@ -20,186 +20,92 @@
         href="https://fonts.googleapis.com/css2?family=Inter&family=Poppins&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
         rel="stylesheet">
 
-        <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="css/login.css">
     <script src="assets/js/menu.js"></script>
 
 </head>
 
 <body>
-<<<<<<< HEAD
-<<<<<<< HEAD:loginAluno.php
 
-
-
-    <header>
-        <button id="botao-hamburguer" onclick="handleMenu()">☰</button>
-
-
-
-
-        <img class="logo" src="assets/img/logo (1).png" alt="">
-
-        <nav>
-            <span>
-                <a href="">Início</a>
-            </span>
-            <span>
-                <a href="">Cursos</a>
-            </span>
-            <span>
-                <a href="">Fale conosco</a>
-            </span>
-            <span>
-                <a href="">Entrar</a>
-            </span>
-        </nav>
-
-
-
-    </header>
-=======
-    
     <?php
-=======
-
-    <?php 
->>>>>>> e5965c205cfb4c973b35fa9d11f0abadd26b684c
         include_once('./components/header.php')
     ?>
->>>>>>> 2bf1aeed1d99ec465fc969c9eb99efc465e92d6a:login.php
 
-        <main>
-            <section>
+    <main>
+        <h1>Faça seu Login</h1>
 
-<<<<<<< HEAD
-        <section class="menu" style="display: none">
-            <div class="menu__top">
-                <button onclick="handleMenu()">
-                    <img src="assets/img/simbolo-x 1.png" alt="">
-                </button>
-            </div>
+        <section class="form-login"">
+            <img src=" assets/img/login.svg" alt="">
 
-            <div class="menu__options">
-                <a href="#">Início</a>
-                <a href="#">Cursos</a>
-                <a href="#">Fale conosco</a>
-                <a href="#" target="">Entrar</a>
-            </div>
-        </section>
+            <form method="POST">
+                <strong>Login</strong>
 
-        <main>
-            <section>
-
-=======
->>>>>>> e5965c205cfb4c973b35fa9d11f0abadd26b684c
-                <section id="poost">
-                    <form method="POST">
-
-                        <div class="main-login">
-                            <h1>
-                                Faça seu Login
-                            </h1>
-
-                            <img src="assets/img/login.svg" alt="">
-
-                            <div class="card-login">
-
-                                <h2>
-                                    LOGIN
-                                </h2>
-
-                                <div class="text--field">
-                                    <label for="usuario">E-mail:</label>
-                                    <input class="in" type="text" name="Usuário" placeholder="Insira seu e-mail">
-                                </div>
-                                <div class="text--field">
-                                    <label for="senha">Senha:</label>
-                                    <input class="in" type="password" name="Senha" placeholder=" Insira sua senha">
-                                </div>
-                                <div class="form-lembrar">
-                                    <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-                                    <label class="form-check-label" for="invalidCheck">
-                                    Lembrar da senha
-                                    </label>
-                                </div>
-
-                                <button class="btn-login">Entrar</button>
-                            </div>
-
-                            <section class="aaa">
-                                <span class ='po'>Ainda não tem cadastro?</span> <a class='cq' href="">Clique aqui</a>
-                            </section>
+                <label for="usuario" class='form-login-label'>E-mail</label>
+                <input type="text" name="usuario" placeholder="Insira seu e-mail" class='form-login-input'>
 
 
-                            <?php
+                <label for="senha" class='form-login-label'>Senha</label>
+                <input type="password" name="senha" placeholder="Insira sua senha" class='form-login-input'>
 
-                            //if(isset($_REQUEST["inserir"]))
-                            //{
-                                //$u = new User();
+                <div class="form-lembrar">
+                    <input type="checkbox" name="invalidCheck">
+                    <label for="invalidCheck">Lembrar da senha</label>
+                </div>
 
-                                //if ($u->autenticarUsuario($_REQUEST["email"],$_REQUEST["senha"]) == 0)
-                                //{
-                                   // echo "<p>E-mail e/ou senha incorreto(s)!</p>";                   
-                                //}
-                                //else 
-                                //{
-                                    //session_start();
-                                    //$_SESSION["nome"] = $u->getNome();
-                                    //header("Location: ../php/void.php"); //*redirecionando para outra página
-                                //}
-                            //}
+                <button class="botao">Entrar</button>
 
-                            if (isset($_REQUEST["inserir"]))
-                            {
-                                $u = new User();
-                
-                                if ($u->autenticarUsuario($_REQUEST["email"], $_REQUEST["senha"]) == 0)
-                                {
-                                    echo "<p>Usuário e/ou senha incorreto(s).</p>";                   
-                                }
-                                else {
-                                    ////Utilizando dados em sessão
-                                    // session_start();
-                                    // $_SESSION["nome"] = $u->getUsuario();
-                                    // header("Location: areaRestrita.php"); //redirecionando para outra página
-                                    $cookieName = "nome";
-                                    $cookieValue = $u->getNome();
-                                    setcookie($cookieName, $cookieValue, time() + 86400, "/");
-                                    header("Location: ../php/void.php");
-                                }
-                            }
-                            
-                
-                        ?>
-
-                    </form>
-
-
+                <section class="aaa">
+                    <span class='po'>Ainda não tem cadastro?</span> <a class='redirecionar' href="cadastro.php">Clique
+                        aqui</a>
                 </section>
-            </section>
-        </main>
-<<<<<<< HEAD
 
-<<<<<<< HEAD:loginAluno.php
-        <footer>
-            <p>
-            © 2023 Braintag - Todos os Direitos Reservados. Desenvolvido com paixão pelo aprendizado.
 
-            </p>
-        </footer>
-=======
+                <?php
+ 
+                    //if(isset($_REQUEST["inserir"]))
+                    //{
+                        //$u = new User();
+ 
+                        //if ($u->autenticarUsuario($_REQUEST["email"],$_REQUEST["senha"]) == 0)
+                        //{
+                            // echo "<p>E-mail e/ou senha incorreto(s)!</p>";                  
+                        //}
+                        //else
+                        //{
+                            //session_start();
+                            //$_SESSION["nome"] = $u->getNome();
+                            //header("Location: ../php/void.php"); //*redirecionando para outra página
+                        //}
+                    //}
+ 
+                    // if (isset($_REQUEST["inserir"]))
+                    // {
+                    //     $u = new User();
+       
+                    //     if ($u->autenticarUsuario($_REQUEST["email"], $_REQUEST["senha"]) == 0)
+                    //     {
+                    //         echo "<p>Usuário e/ou senha incorreto(s).</p>";                  
+                    //     }
+                    //     else {
+                    //         ////Utilizando dados em sessão
+                    //         // session_start();
+                    //         // $_SESSION["nome"] = $u->getUsuario();
+                    //         // header("Location: areaRestrita.php"); //redirecionando para outra página
+                    //         $cookieName = "nome";
+                    //         $cookieValue = $u->getNome();
+                    //         setcookie($cookieName, $cookieValue, time() + 86400, "/");
+                    //         header("Location: ../php/void.php");
+                    //     }
+                    // }
+                ?>
+            </form>
+        </section>
+    </main>
+
     <?php
         include_once('./components/footer.php')
     ?>
->>>>>>> 2bf1aeed1d99ec465fc969c9eb99efc465e92d6a:login.php
-
-=======
->>>>>>> e5965c205cfb4c973b35fa9d11f0abadd26b684c
-
-        <?php 
-            include_once('./components/footer.php')
-        ?>
 </body>
 
 </html>
